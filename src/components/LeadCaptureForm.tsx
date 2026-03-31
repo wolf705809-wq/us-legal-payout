0c1b9dd74b89b7fd2f72ed0a7b2c9b3fd82707e1
강다현 <kdh@gangdahyeon-ui-MacBookAir.local>
2026-04-01 01:13:37 +0900

    dd

--- src/components/LeadCaptureForm.tsx
+++ src/components/LeadCaptureForm.tsx
@@ -139,22 +139,6 @@ export default function LeadCaptureForm({
 
   return (
     <>
-    {/* TrustedForm */}
-    <Script
-      id="trustedform"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `(function() {
-  var tf = document.createElement('script');
-  tf.type = 'text/javascript'; tf.async = true;
-  tf.src = ("https:" == document.location.protocol ? 'https' : 'http') +
-    '://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=' +
-    new Date().getTime() + Math.random();
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(tf, s);
-})();`,
-      }}
-    />
     {/* Cloudflare Turnstile */}
     {process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY && (
       <Script
