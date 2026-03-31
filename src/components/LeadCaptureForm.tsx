cbae40cece6c22512a2456ca7b8e9bed6af34087
wolf705809-wq <wolf705809@gmail.com>
2026-03-31 23:16:36 +0900

    Update LeadCaptureForm.tsx

--- src/components/LeadCaptureForm.tsx
+++ src/components/LeadCaptureForm.tsx
@@ -147,9 +147,8 @@ export default function LeadCaptureForm({
         __html: `(function() {
   var tf = document.createElement('script');
   tf.type = 'text/javascript'; tf.async = true;
-  tf.src = ("https:" == document.location.protocol ? 'https' : 'http') +
-    '://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=' +
-    new Date().getTime() + Math.random();
+  tf.src = 'https://api.trustedform.com/trustedform.js?field=xxTrustedFormCertUrl&use_tagged_consent=true&l=' +
+  new Date().getTime() + Math.random();
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(tf, s);
 })();`,
